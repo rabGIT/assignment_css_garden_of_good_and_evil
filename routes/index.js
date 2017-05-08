16b9b04e153e83e11292424aefa6cb18d926a463
rabGIT <rabGIT@users.noreply.github.com>
2017-05-08 11:36:06 -0700

    cookie based config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let morals = req.cookies.morals;
+  if (!morals) morals = 'good';
+  console.log(req.cookies);
+  let flash = req.cookies.insanity ? (1 / parseInt(req.cookies.insanity)) : 0;
+  if (flash == 1) flash = 0;
+  res.render('index', { title: 'CSS Garden of Good and Evil',
+                        morals: (morals === 'good'),
+                        favFood: req.cookies.favFood || 'Favorite Food',
+                        favColor: req.cookies.favColor || 'black',
+                        flash: flash.toString() + 's',
+                        insanity: req.cookies.insanity || 1
+                      });
+});
+
+/* Post home page. */
+router.post('/', function(req, res, next) {
+  res.cookie("morals", req.body.morals);
+  res.cookie("favFood", req.body.favFood || req.cookies.favFood);
+  res.cookie("favColor", req.body.favColor || req.cookies.favColor);
+  res.cookie("insanity", req.body.insanity || req.cookies.insanity)
+  res.redirect("back");
 });
 
 module.exports = router;
